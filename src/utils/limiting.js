59e9f905eb1d69dd55973c062261d3e1ad1652e9
tester620 <dheerajverma82664442@gmail.com>
2025-08-09 17:45:17 +0530

    Update limiting.js

--- src/utils/limiting.js
+++ src/utils/limiting.js
@@ -2,8 +2,16 @@ import { rateLimit } from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 5,
+  max: 100,
   message: {
     message: "You're making requests too quickly. Please wait a minute and try again.",
+  },
+});
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 15,
+  message: {
+    message: "You're making requests too quickly. Please wait for some time and try again.",
   },
 });
